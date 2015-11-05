7fbb631d809a49df19ce118a437b2ee965d49ac7
oeuillot <olivier.oeuillot@gmail.com>
2015-11-05 15:16:43 +0100

    Fix code

--- hangouts-bot.js
+++ hangouts-bot.js
@@ -8,20 +8,21 @@ var util = require('util');
 function HangoutsBot(username, password, host, onlineStatus) {
   Events.EventEmitter.call(this);
 
-  var that = this;
+  var self = this;
 
-  this.connection = connection = new xmpp.Client({
+  var connection = new xmpp.Client({
     jid : username,
     password : password,
     host : "talk.google.com"
   });
+  this.connection = connection;
 
-  this.connection.on('online', function() {
+  connection.on('online', function() {
     connection.send(new xmpp.Element('presence', {}).c('show').t('chat').up()
         .c('status').t(onlineStatus || 'Online'));
 
     var roster_elem = new xmpp.Element('iq', {
-      'from' : connection.jid,
+      'from' : self.connection.jid,
       'type' : 'get',
       'id' : 'google-roster'
     }).c('query', {
@@ -32,13 +33,13 @@ function HangoutsBot(username, password, host, onlineStatus) {
 
     connection.send(roster_elem);
 
-    that.emit('online');
+    self.emit('online');
   });
 
-  this.connection.on('stanza', function(stanza) {
+  connection.on('stanza', function(stanza) {
     if (stanza.is('message') && (stanza.attrs.type !== 'error') &&
         (stanza.getChildText('body'))) {
-      that.emit('message', stanza.attrs.from, stanza.getChildText('body'));
+      self.emit('message', stanza.attrs.from, stanza.getChildText('body'));
     }
 
     if (stanza.is('presence') && stanza.attrs.type === 'subscribe') {
@@ -49,20 +50,18 @@ function HangoutsBot(username, password, host, onlineStatus) {
     }
   });
 
-  this.connection.on('error', function(e) {
-    console.log(e);
+  connection.on('error', function(e) {
+    console.error(e);
   });
+}
 
-  this.sendMessage = function(to, message) {
-    var stanza = new xmpp.Element('message', {
-      to : to,
-      type : 'chat'
-    }).c('body').t(message);
+HangoutsBot.prototype.sendMessage = function(to, message) {
+  var stanza = new xmpp.Element('message', {
+    to : to,
+    type : 'chat'
+  }).c('body').t(message);
 
-    this.connection.send(stanza);
-  }
-
-  return this;
+  this.connection.send(stanza);
 }
 
 util.inherits(HangoutsBot, Events.EventEmitter);
